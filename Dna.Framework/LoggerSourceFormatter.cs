0de33329a947e4381bf5c4ed13cc0bae08061494
Olonyl Landeros <jose87.landeros@gmail.com>
2024-01-04 14:24:47 -0600

    First commit

---
+++ Dna.Framework/LoggerSourceFormatter.cs
@@ -0,0 +1,20 @@
+﻿namespace Dna.Framework
+{
+    public static class LoggerSourceFormatter
+    {
+        public static string Format(object[] state, Exception exception)
+        {
+            var origin = (string)state[0];
+            var filePath = (string)state[1];
+            var lineNumber = (string)state[2];
+            var message = (string)state[3];
+
+            var exceptionMessage = exception?.ToString();
+
+            if (exceptionMessage != null)
+                exceptionMessage = Environment.NewLine + exception;
+
+            return $"{message} [{Path.GetFileName(filePath)} > {origin}() > {lineNumber}]{exceptionMessage}";
+        }
+    }
+}
